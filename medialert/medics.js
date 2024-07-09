5a72b133fa71d4166b68447c40770dacac81dcaa
codersoum <22h51a0558@cmrcet.ac.in>
2024-07-09 21:47:39 +0530

    Initial commit with medialert and servmed.js

---
+++ medialert/medics.js
@@ -0,0 +1,8 @@
+function showlogin()
+ {
+   window.location.href="http://localhost:5050/login.html"
+ }
+ function showsignup()
+ {
+   window.location.href="http://localhost:5050/register.html"
+ }
