31f32e35e2a1896a8723b00f155535d653a58101
codersoum <150548307+codersoum@users.noreply.github.com>
2024-07-09 20:10:51 +0530

    Delete medics.js

--- medics.js
+++
@@ -1,8 +0,0 @@
-function showlogin()
- {
-   window.location.href="http://localhost:5050/login.html"
- }
- function showsignup()
- {
-   window.location.href="http://localhost:5050/register.html"
- }
